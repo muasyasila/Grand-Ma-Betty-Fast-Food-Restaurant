c2769ad1b7948ac73f15914b2e0f5788c3f4b7a3
muasyasila <curtissila4@gmail.com>
2023-06-30 23:42:13 +0300

    More files added

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,7 +1,7 @@
 <!--footer starts-->
 <div class="footer">
            <div class="wrapper">
-               <p class="text-center">2021 All right reserved, Grand Ma' BETTY Fast Food Resturant. Developed by - <a href="#"> Curtis Muasya </a><p>
+               <p class="text-center">2023 All right reserved, Grand Ma' BETTY Fast Food Resturant. Developed by - <a href="#"> Curtis Muasya </a><p>
            </div>
 </div>
 <!--Footer Section Ends-->
